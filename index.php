881a46b3fe0977b1a628dd35e0b4f8b6887580b4
Jonás <jonasur@gmail.com>
2012-07-25 10:17:51 +0200

    Problema con login fb

--- index.php
+++ index.php
@@ -47,7 +47,8 @@ $jugador = $repositorioJugadores->getJugador($idFacebook);
             type="text/javascript"  ></script>
         <script type="text/javascript" src="scripts/script.js"></script>
         <script type="text/javascript" src="scripts/script2.js"></script>
-
+        <script type="text/javascript" src="scripts/fb.js"></script>
+        
     </head>
     <body>
         <div id=fb-root"></div>
@@ -56,28 +57,20 @@ $jugador = $repositorioJugadores->getJugador($idFacebook);
 
     window.fbAsyncInit = function() {
         FB.init({
-          appId      : '<?php echo AppInfo::appID(); ?>', // App ID
-          channelUrl : '//<?php echo $_SERVER["HTTP_HOST"]; ?>/channel.html', // Channel File
-          status     : true, // check login status
-          cookie     : true, // enable cookies to allow the server to access the session
-          xfbml      : true // parse XFBML
+          appId      : '<?php echo AppInfo::appID(); ?>',
+          channelUrl : '//<?php echo $_SERVER["HTTP_HOST"]; ?>/channel.html',
+          status     : true,
+          cookie     : true,
+          xfbml      : true
         });
 
-        // Listen to the auth.login which will be called when the user logs in
-        // using the Login button
         FB.Event.subscribe('auth.login', function(response) {
-          // We want to reload the page now so PHP can read the cookie that the
-          // Javascript SDK sat. But we don't want to use
-          // window.location.reload() because if this is in a canvas there was a
-          // post made to this page and a reload will trigger a message to the
-          // user asking if they want to send data again.
           window.location = window.location;
         });
 
         FB.Canvas.setAutoGrow();
       };
 
-      // Load the SDK Asynchronously
       (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
