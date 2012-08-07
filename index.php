8a8c47169d66c7a01ff2626bd3a1d6dfc0b94622
Jonás <jonasur@gmail.com>
2012-08-07 02:11:56 +0200

    Esto es muy raro

--- index.php
+++ index.php
@@ -17,7 +17,6 @@
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"
         type="text/javascript"  ></script>
-
         <script type="text/javascript" src="scripts/script.js"></script>
 
 
